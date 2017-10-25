7783038d99107720baac618e939814bcf97f6fe8
Ed Lowther <ed.lowther[at]gmail.com>
2017-10-25 17:23:29 +0100

    Business logic

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -7,9 +7,23 @@ describe("Thermostat", function() {
     expect(thermostat.temperature()).toEqual(20);
   });
   it('increases the temperature with an up function', function() {
-    thermostat.up(10);
-    expect(thermostat.temperature()).toEqual(30)
+    thermostat.up(3);
+    expect(thermostat.temperature()).toEqual(23)
   });
+  it('has a maximimum temperature of 25 degrees if power_saving_mode is on', function() {
+    thermostat.up(6);
+    expect(thermostat.temperature()).toEqual(25);
+  })
+  it('can be set to above 25 degrees if power_saving_mode is off', function() {
+    thermostat.set_power_saving_mode(false);
+    thermostat.up(11);
+    expect(thermostat.temperature()).toEqual(31);
+  })
+  it('has a maximimum temperature of 32 degrees if power_saving_mode is off', function() {
+    thermostat.set_power_saving_mode(false);
+    thermostat.up(13);
+    expect(thermostat.temperature()).toEqual(32);
+  })
   it('decreases the temperature with a down function', function() {
     thermostat.down(8);
     expect(thermostat.temperature()).toEqual(12)
@@ -18,5 +32,38 @@ describe("Thermostat", function() {
     thermostat.down(11);
     expect(thermostat.temperature()).toEqual(10)
   });
+  it('has a power saving mode that defaults to on', function() {
+    expect(thermostat.power_saving_mode()).toEqual(true);
+  });
+  it('has a way to turn power saving mode off', function() {
+    thermostat.set_power_saving_mode(false);
+    expect(thermostat.power_saving_mode()).toEqual(false);
+  })
+  it('power saving mode off can be switched back on', function() {
+    thermostat.set_power_saving_mode(false);
+    thermostat.set_power_saving_mode(true);
+    expect(thermostat.power_saving_mode()).toEqual(true);
+  })
+  it('can be reset to 20 degrees', function() {
+    thermostat.up(13);
+    thermostat.reset_temperature();
+    expect(thermostat.temperature()).toEqual(20);
+  })
+  it('claims to be low energy if the temperature is below 18', function() {
+    thermostat.down(3);
+    expect(thermostat.energy_usage()).toEqual("low-usage");
+  })
+  it('claims to be medium energy if the temperature is 18', function() {
+    thermostat.down(2);
+    expect(thermostat.energy_usage()).toEqual("medium-usage");
+  })
+  it('claims to be medium energy if the temperature is 24', function() {
+    thermostat.up(4);
+    expect(thermostat.energy_usage()).toEqual("medium-usage");
+  })
+  it('claims to be high energy if the temperature is 25', function() {
+    thermostat.up(5);
+    expect(thermostat.energy_usage()).toEqual("high-usage");
+  })
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,21 +1,56 @@
-
 function Thermostat() {
-  this._MINIMUM_TEMPERATURE = 10
+  this._MINIMUM_TEMPERATURE = 10;
   this._temperature = 20;
-}
+  this._power_saving_mode = true;
+};
 
-Thermostat.prototype.temperature = function(){
-  return this._temperature
+Thermostat.prototype.temperature = function() {
+  return this._temperature;
+};
+
+Thermostat.prototype.power_saving_mode = function() {
+  return this._power_saving_mode;
+};
+
+Thermostat.prototype.set_power_saving_mode = function(true_or_false) {
+  this._power_saving_mode = true_or_false;
+};
+
+Thermostat.prototype._maximum_temperature = function() {
+  if (this._power_saving_mode) {
+    return 25;
+  } else {
+    return 32;
+  }
 };
 
 Thermostat.prototype.up = function(number) {
-  this._temperature += number
+  if ((this._temperature + number) < this._maximum_temperature()) {
+    this._temperature += number;
+  } else {
+    this._temperature = this._maximum_temperature();
+  }
 };
 
 Thermostat.prototype.down = function(number) {
-  if((this._temperature - number) > this._MINIMUM_TEMPERATURE){
-  this._temperature -= number;
-}else{
-  this._temperature = this._MINIMUM_TEMPERATURE;
-}
+  if ((this._temperature - number) > this._MINIMUM_TEMPERATURE) {
+    this._temperature -= number;
+  } else {
+    this._temperature = this._MINIMUM_TEMPERATURE;
+  }
+};
+
+Thermostat.prototype.reset_temperature = function() {
+  this._temperature = 20;
+};
+
+Thermostat.prototype.energy_usage = function() {
+  if (this._temperature < 18) {
+    return "low-usage";
+  } else if (this._temperature < 25) {
+    return "medium-usage";
+  } else {
+    return "high-usage";
+  }
+
 };
