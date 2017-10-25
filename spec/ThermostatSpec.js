5b354a95b252d236b08e65ec39aec19941be3db1
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-10-25 16:31:50 +0100

    Adds up function for adding temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,6 +1,11 @@
 describe("Thermostat", function() {
   var thermostat = new Thermostat();
   it('starts at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20);
-  }); 
+    expect(thermostat.temperature()).toEqual(20);
+  });
+  it('increases the temperature with an up function', function() {
+    thermostat.up(10);
+    expect(thermostat.temperature()).toEqual(30)
+  });
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,11 @@
 function Thermostat() {
-  this.temperature = 20;
+  this._temperature = 20;
 }
+
+Thermostat.prototype.temperature = function(){
+  return this._temperature
+};
+
+Thermostat.prototype.up = function(number) {
+  this._temperature += number
+};
